48510384bb20cf9ea3de388775c2c220c7b243e3
Marceli Jankowski <marcelijankowski123@gmail.com>
2022-07-06 13:11:49 +0200

    handling SIGINT and SIGTERM signals in production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 // PROJECT_MODULES
 import app from "./app";
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Server is up and running");
 });
+
+// docker containers use SIGINT (ctrl-c) and SIGTERM (docker stop) signals to properly exit
+// node by default doesn't handle SIGINT/SIGTERM, so I need to handle them manually
+// (npm is not propagating these signals so it works only with node as the main process)
+process.on("SIGINT", () => gracefullShutDown("SIGINT"));
+process.on("SIGTERM", () => gracefullShutDown("SIGTERM"));
+
+function gracefullShutDown(signal: string): void {
+  console.log(signal, " Graceful shutdown ", new Date().toISOString());
+
+  server.close(() => {
+    console.log("SERVER IS DOWN");
+    process.exit(0);
+  });
+}

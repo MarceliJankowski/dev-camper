85af2e10441c9c415614eb1f8a9b00307203acbb
Marceli Jankowski <marcelijankowski123@gmail.com>
2022-07-06 20:06:52 +0200

    adding uncaughtException and unhandledRejection listeners / handlers

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
+// uncaughtException listener needs to be executed before any other code
+// this way it can it can start listening for exceptions before any other code runs
+process.on("uncaughtException", (err: Error) => {
+  console.error("SERVER IS DOWN");
+  console.error("UNCAUGHT EXCEPTION");
+  console.error(err);
+  process.exit(1);
+});
+
 // PROJECT_MODULES
 import app from "./app";
+import { logErr } from "./utils";
 
 const server = app.listen(5000, () => {
   console.log("Server is up and running");
+});
+
+// EVENT LISTENERS
+
+process.on("unhandledRejection", (err: Error) => {
+  logErr("unhandled promise rejection", err);
+
+  server.close(() => {
+    logErr("server is down");
+    process.exit(2);
+  });
 });
 
 // docker containers use SIGINT (ctrl-c) and SIGTERM (docker stop) signals to properly exit

95a53b2d1166cb87b7e27172f859a09919c89b4f
Marceli Jankowski <marcelijankowski123@gmail.com>
2022-07-06 21:03:03 +0200

    adding morgan package and mounting middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 // PACKAGES
 import express from "express";
+import morgan from "morgan";
 
 // PROJECT_MODULES
 import { bootcampRouter } from "./routes";
 import { BOOTCAMPS_URL } from "./constants";
+import { getEnvVar } from "./utils";
 
 const app = express();
 export default app;
 
+// MOUNT MIDDLEWARES
+app.use(express.json());
+
+const NODE_ENV = getEnvVar("NODE_ENV");
+if (NODE_ENV === "development") app.use(morgan("dev"));
+
+// MOUNT ROUTERS
 app.use(BOOTCAMPS_URL, bootcampRouter);

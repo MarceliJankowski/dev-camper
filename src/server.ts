d96eb8d9bbc17c90a85c511eb9a3d6d4799bc262
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-06-15 18:13:16 +0200

    handling 'INT' and 'TERM' signals

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 // MODULES
 import app from "./app";
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Server is up and running");
 });
+
+// handling 'INT' and 'TERM' signals
+process.on("SIGINT", () => gracefulShutdown("INT"));
+process.on("SIGTERM", () => gracefulShutdown("TERM"));
+
+function gracefulShutdown(signal: string): void {
+  console.log(signal, "signal received -", new Date());
+  console.log("Gracefully shutting down...");
+
+  server.close(() => {
+    console.log("SERVER IS DOWN");
+    process.exit(0);
+  });
+}

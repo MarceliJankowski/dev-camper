760f3a1ef620f99d221e7697f864b249a5ff2a36
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-06-17 14:38:49 +0200

    handling 'uncaughtException' and 'unhandledRejection' events

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
+// UNCAUGHT EXCEPTION
+process.on("uncaughtException", (err: unknown) => {
+  console.error("UNCAUGHT EXCEPTION -", new Date());
+  console.error(err);
+  console.error("SERVER IS DOWN");
+  process.exit(1);
+});
+
 // MODULES
 import app from "./app";
 
 const server = app.listen(5000, () => {
   console.log("Server is up and running");
+});
+
+// UNHANDLED PROMISE REJECTION
+process.on("unhandledRejection", (value: unknown) => {
+  console.error("UNHANDLED PROMISE REJECTION -", new Date());
+  console.error(value);
+
+  server.close(() => {
+    console.error("SERVER IS DOWN");
+    process.exit(2);
+  });
 });
 
 // handling 'INT' and 'TERM' signals

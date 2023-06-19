e43308c1d083877fbd1bd7a5e86d87852908ed4f
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-06-19 15:28:57 +0200

    adding supertest package and testing '/health' endpoint

---
+++ src/app.spec.ts
@@ -0,0 +1,14 @@
+// PACKAGES
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+
+// PROJECT_MODULES
+import app from "./app";
+
+describe("GET /health", () => {
+  it("responds with expected headers and body", async () => {
+    const { body } = await request(app).get("/health").expect(200).expect("Content-Type", /json/);
+
+    expect(body.status).toBe("OK");
+  });
+});

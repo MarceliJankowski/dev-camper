deae7b3d912c160bc53d10efd388e14838abb37a
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-06-28 23:28:30 +0200

    adding morgan package, mounting 'express.json' and 'morgan' middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 // PACKAGES
 import express from "express";
+import morgan from "morgan";
+
+// MODULES
+import { getEnvVar } from "./utils";
+
+const NODE_ENV = getEnvVar("NODE_ENV");
 
 const app = express();
 export default app;
+
+// MIDDLEWARES
+app.use(express.json());
+
+app.use(
+  morgan(NODE_ENV === "production" ? "common" : "dev", {
+    skip: req => req.url === "/health",
+  })
+);
 
 // HEALTH END-POINT
 app.get("/health", (_, res) => {

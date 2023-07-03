31e14297c11a32e07f8270238e3aaafd44e9654c
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-07-03 20:43:02 +0200

    adding IntentionalError utility

---
+++ src/utils/intentionalError.spec.ts
@@ -0,0 +1,56 @@
+// PACKAGES
+import { it, expect } from "vitest";
+
+// MODULES
+import { IntentionalError } from "./intentionalError";
+
+const inputMessage = "input message";
+
+it("instantiated holds expected properties", () => {
+  const inputStatusCode = 500;
+  const expectedProperties = new Map();
+  expectedProperties.set("message", inputMessage);
+  expectedProperties.set("statusCode", inputStatusCode);
+  expectedProperties.set("status", "error");
+  expectedProperties.set("isIntentional", true);
+
+  const testErr = new IntentionalError(inputMessage, inputStatusCode);
+
+  expectedProperties.forEach((value, key) => {
+    expect(testErr).toHaveProperty(key);
+    expect(testErr[key]).toBe(value);
+  });
+
+  expect(testErr).toHaveProperty("stack");
+});
+
+it("correctly generates status property based on statusCode argument", () => {
+  const [inputStatusCode1, inputStatusCode2] = [400, 500];
+  const [expectedStatus1, expectedStatus2] = ["fail", "error"];
+
+  const testErr1 = new IntentionalError(inputMessage, inputStatusCode1);
+  const testErr2 = new IntentionalError(inputMessage, inputStatusCode2);
+
+  expect(testErr1.status).toBe(expectedStatus1);
+  expect(testErr2.status).toBe(expectedStatus2);
+});
+
+it("is an instance of Error Constructor", () => {
+  const inputStatusCode = 500;
+
+  const testErr = new IntentionalError(inputMessage, inputStatusCode);
+
+  expect(testErr).toBeInstanceOf(Error);
+});
+
+it(`raises exception when instantiated with invalid statusCode`, () => {
+  const invalidStatusCode1 = 600;
+  const invalidStatusCode2 = 399;
+  const expectedErrMessage = /statusCode: '.*' is invalid/;
+
+  const testFn1 = () => new IntentionalError(inputMessage, invalidStatusCode1);
+  const testFn2 = () => new IntentionalError(inputMessage, invalidStatusCode2);
+
+  expect(testFn1).toThrow(expectedErrMessage);
+  expect(testFn2).toThrow(expectedErrMessage);
+});
---
+++ src/utils/intentionalError.ts
@@ -0,0 +1,23 @@
+/**@desc Constructor instantiated inside of middlewares whenever error should reach the client
+@param message functionally the same as `Error()` message
+@param statusCode used for deriving `status` property (valid range: 400-599 including)*/
+export class IntentionalError extends Error {
+  /**@desc indication to `globalErrorHandler` that this error should reach the client (in development and production)*/
+  public readonly isIntentional: true;
+
+  /**@desc differentiates between internal server `"error"` (server's fault) and `"fail"` (client's fault). Derived from `statusCode`*/
+  public readonly status: StatusErrType;
+
+  constructor(public readonly message: string, public readonly statusCode: number) {
+    if (statusCode < 400 || statusCode >= 600)
+      throw new Error(`statusCode: '${statusCode}' is invalid (valid range: 400-599 including)`);
+
+    super(message);
+
+    this.isIntentional = true;
+    this.status = statusCode < 500 ? "fail" : "error";
+
+    // start stack trace with IntentionalError invocation
+    Error.captureStackTrace(this, this.constructor);
+  }
+}

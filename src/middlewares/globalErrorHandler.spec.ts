9291f76cac91458df257460114030c2a4d9f40d9
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-07-10 18:07:37 +0200

    updating globalErrorHandler, handling mongoose 'ValidationError', 'CastError' and MongoDB '11000' errors

--- src/middlewares/globalErrorHandler.spec.ts
+++ src/middlewares/globalErrorHandler.spec.ts
@@ -1,5 +1,6 @@
 // PACKAGES
 import { it, vi, expect, describe, afterEach } from "vitest";
+import mongoose from "mongoose";
 
 // MODULES
 import { IntentionalError } from "../utils";
@@ -17,7 +18,23 @@ describe("new SendError()", () => {
     status: resStubStatus,
   };
 
-  it("development - responds with all available data, automatically infers 'status' and 'statusCode' when not present", () => {
+  it("development - responds with all available data", () => {
+    vi.stubEnv("NODE_ENV", "development");
+    const inputErr = new IntentionalError("test error message", 400);
+    const expectedResBody = {
+      status: "fail",
+      message: inputErr.message,
+      error: inputErr,
+      stack: inputErr.stack,
+    };
+
+    new SendError(inputErr, resStub);
+
+    expect(resStubStatus).toBeCalledWith(inputErr.statusCode);
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
+  });
+
+  it("development - automatically infers 'status' and 'statusCode' when not present", () => {
     vi.stubEnv("NODE_ENV", "development");
     const inputErr = new Error("test error message");
     const expectedStatusCode = 500;
@@ -26,12 +43,19 @@ describe("new SendError()", () => {
     new SendError(inputErr, resStub);
 
     expect(resStubStatus).toBeCalledWith(expectedStatusCode);
-    expect(resStubJSON).toBeCalledWith({
-      status: expectedStatus,
-      message: inputErr.message,
-      error: inputErr,
-      stack: inputErr.stack,
-    });
+    expect(resStubJSON).toBeCalledWith(expect.objectContaining({ status: expectedStatus }));
+  });
+
+  it("development - standardizes primitive err argument by wrapping it in Error constructor", () => {
+    vi.stubEnv("NODE_ENV", "development");
+    const inputPrimitiveErr = "test error";
+    const expectedError = new Error(inputPrimitiveErr);
+
+    new SendError(inputPrimitiveErr, resStub);
+
+    expect(resStubJSON).toBeCalledWith(
+      expect.objectContaining({ message: expectedError.message, error: expectedError })
+    );
   });
 
   it("raises exception when 'NODE_ENV' env variable is invalid", () => {
@@ -48,29 +72,88 @@ describe("new SendError()", () => {
     vi.stubEnv("NODE_ENV", "production");
     const inputErr = new Error("test error message");
     const expectedStatusCode = 500;
-    const expectedStatus = "error";
-    const expectedMessage = "something went wrong";
+    const expectedResBody = {
+      status: "error",
+      message: "something went wrong",
+    };
 
     new SendError(inputErr, resStub);
 
     expect(resStubStatus).toBeCalledWith(expectedStatusCode);
-    expect(resStubJSON).toBeCalledWith({
-      status: expectedStatus,
-      message: expectedMessage,
-    });
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
   });
 
   it("production - responds with actual data when error is an instance of IntentionalError", () => {
     vi.stubEnv("NODE_ENV", "production");
     const inputErrStatusCode = 400;
     const inputErr = new IntentionalError("test error message", inputErrStatusCode);
+    const expectedResBody = {
+      status: inputErr.status,
+      message: inputErr.message,
+    };
 
     new SendError(inputErr, resStub);
 
     expect(resStubStatus).toBeCalledWith(inputErrStatusCode);
-    expect(resStubJSON).toBeCalledWith({
-      status: inputErr.status,
-      message: inputErr.message,
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
+  });
+
+  it("production - handles mongoose CastError", () => {
+    vi.stubEnv("NODE_ENV", "production");
+    const inputCastError = new mongoose.Error.CastError("ObjectId", "uncoercible/invalid id", "_id");
+    const expectedStatusCode = 400;
+    const expectedResBody = {
+      status: "fail",
+      message: `invalid id: '${inputCastError.value}' passed to '${inputCastError.path}' field`,
+    };
+
+    new SendError(inputCastError, resStub);
+
+    expect(resStubStatus).toBeCalledWith(expectedStatusCode);
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
+  });
+
+  it("production - handles mongoose ValidationError", async () => {
+    vi.stubEnv("NODE_ENV", "production");
+    const inputErrors = ["first error message", "second error message"];
+    const inputValidationError = new mongoose.Error.ValidationError();
+    inputValidationError.errors = {
+      field1: new mongoose.Error.ValidatorError({ message: inputErrors[0] }),
+      field2: new mongoose.Error.ValidatorError({ message: inputErrors[1] }),
+    };
+    const expectedStatusCode = 400;
+    const expectedResBody = {
+      status: "fail",
+      message: inputErrors.join(", "),
+    };
+
+    console.log(inputValidationError instanceof mongoose.Error.ValidatorError);
+
+    new SendError(inputValidationError, resStub);
+
+    expect(resStubStatus).toBeCalledWith(expectedStatusCode);
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
+  });
+
+  it("production - handles MongoDB 11000 (duplicate value for a field with unique index) error", () => {
+    vi.stubEnv("NODE_ENV", "production");
+    const inputFieldName = "name";
+    const inputFieldValue = "fake duplicate value";
+    const inputError = Object.assign(new Error(), {
+      code: 11000,
+      keyValue: {
+        [inputFieldName]: inputFieldValue,
+      },
     });
+    const expectedStatusCode = 400;
+    const expectedResBody = {
+      status: "fail",
+      message: `field '${inputFieldName}' received duplicate value: '${inputFieldValue}'`,
+    };
+
+    new SendError(inputError, resStub);
+
+    expect(resStubStatus).toBeCalledWith(expectedStatusCode);
+    expect(resStubJSON).toBeCalledWith(expectedResBody);
   });
 });
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,26 +1,27 @@
 // PACKAGES
 import { ErrorRequestHandler, Response } from "express";
+import mongoose from "mongoose";
 
 // MODULES
 import { IntentionalError, getEnvVar } from "../utils";
 
-interface ExtendedError extends Error {
-  statusCode: number;
-  status: Status;
-  isIntentional?: true;
+interface MongoDuplicateValueError {
+  code: number;
+  keyValue: { [key: string]: unknown };
 }
+
+type StandardizedError = Error & Partial<IntentionalError & MongoDuplicateValueError>;
 
 /**@desc sends exception to the client based on `NODE_ENV`:
 @development exposes all data
-@production responds only with data deemed "safe" (security breach prevention)*/
+@production only responds with data deemed "safe" (security breach prevention)*/
 export class SendError {
-  private err: ExtendedError;
+  private err: StandardizedError;
 
-  constructor(err: Error | ExtendedError, private readonly res: Response) {
-    // these properties not being defined indicate that it's a server internal error, so treat it as such
-    (err as ExtendedError).statusCode ??= 500;
-    (err as ExtendedError).status ??= "error";
-    this.err = err as ExtendedError;
+  constructor(err: unknown, private readonly res: Response) {
+    // standardize err argument by wrapping it in Error
+    if (!(err instanceof Error)) this.err = new Error(String(err));
+    else this.err = err;
 
     this.sendErrorBasedOnNodeEnv();
   }
@@ -46,16 +47,52 @@ export class SendError {
   }
 
   private sendErrorInDevelopment() {
-    this.res.status(this.err.statusCode).json({
-      status: this.err.status,
+    this.res.status(this.err.statusCode ?? 500).json({
+      status: this.err.status ?? "error",
       message: this.err.message,
       error: this.err,
       stack: this.err.stack,
     });
   }
 
+  private setError(message: string, statusCode: number = 400) {
+    this.err = new IntentionalError(message, statusCode);
+  }
+
+  /**@desc handles `MongoDB` collection receiving duplicate value for a field with unique index on it*/
+  private handleMongoDuplicateValue() {
+    if (this.err.code !== 11000) return;
+    const mongoError = this.err as MongoDuplicateValueError;
+
+    const [field, value] = Object.entries(mongoError.keyValue)[0];
+    this.setError(`field '${field}' received duplicate value: '${value}'`);
+  }
+
+  /**@desc handles `mongoose` receiving invalid document id*/
+  private handleMongooseInvalidDocumentId() {
+    if (!(this.err instanceof mongoose.Error.CastError)) return;
+
+    this.setError(`invalid id: '${this.err.value}' passed to '${this.err.path}' field`);
+  }
+
+  /**@desc handles `mongoose` receiving value with invalid type (for instance `number` field receiving a `string`)*/
+  private handleMongooseInvalidType() {
+    if (!(this.err instanceof mongoose.Error.ValidationError)) return;
+
+    const preparedErrMessage = Object.values(this.err.errors)
+      .map(({ message }) => message)
+      .join(", ");
+
+    this.setError(preparedErrMessage);
+  }
+
   private sendErrorInProduction() {
-    // expose actual data in case it's an IntentionalError
+    // HANDLE PACKAGE SPECIFIC EXCEPTIONS
+    this.handleMongoDuplicateValue();
+    this.handleMongooseInvalidDocumentId();
+    this.handleMongooseInvalidType();
+
+    // expose actual data in case it's an IntentionalError (includes handled package exceptions)
     if (this.err instanceof IntentionalError) {
       this.res.status(this.err.statusCode).json({
         status: this.err.status,
